fed0ebb53d73335a32d0dead2ccae9d87884fcba
Ali imran Bozkurt <albozkur@Ali-MacBook-Air.local>
2025-01-13 15:35:33 +0300

    fbv

--- Project/Django/srcs/users/urls.py
+++ Project/Django/srcs/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import UserViewSet, UserDeleteView
+from .views import UserViewSet, UserDeleteView, user_list, user_create, user_update
 
 urlpatterns = [
+	path('put/<int:pk>', user_update, name='user-update'),
+	path('get/', user_list, name='user-list'),
+    path('post/', user_create, name='user-create'),
     path('users/', UserViewSet.as_view(), name='users'),
     path('users/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),
 ]
--- Project/Django/srcs/users/views.py
+++ Project/Django/srcs/users/views.py
@@ -3,11 +3,55 @@ from .models import User
 from .serializers import UserSerializer
 from rest_framework import generics
 
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+
+
+def user_list(request):
+	if request.method != "GET":
+		return JsonResponse({"error": "Only GET method is allowed."}, status=405)
+	try:
+		obj = User.objects.all()
+		serializer = UserSerializer(obj, many=True)
+		return JsonResponse(serializer.data, safe=False)
+	except Exception as e:
+		return JsonResponse({"error": str(e)}, status=500)
+
+@csrf_exempt
+def user_create(request):
+	if request.method != "POST":
+		return JsonResponse({"error": "Only POST method is allowed."}, status=405)
+	try:
+		data = JSONParser().parse(request)
+		serializer = UserSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=201)
+		return JsonResponse(serializer.errors, status=400)
+
+	except Exception as e:
+		return JsonResponse({"error": str(e)}, status=500)
+
+@csrf_exempt
+def user_update(request, pk):
+	if request.method != "PUT":
+		return JsonResponse({"error": "Only PUT method is allowed."}, status=405)
+	try:
+		user = User.objects.get(pk=pk)
+		data = JSONParser().parse(request)
+		serializer = UserSerializer(user, data=data)
+		if (serializer.is_valid()):
+			serializer.save()
+			return (JsonResponse(serializer.data, status=200))
+		return JsonResponse(serializer.errors, status=400)
+	except Exception as e:
+		return JsonResponse({"error": str(e)}, status=500)
 
 class UserViewSet(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
 
 class UserDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
